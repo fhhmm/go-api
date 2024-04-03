93f3b49ed0ac02931b01b9baca77f3c759ae676d
toshitk <takahashi.t66@gmail.com>
2024-04-03 15:56:21 +0900

    refactor: aggregated handlers

--- src/handler/hello_handler.go
+++ src/handler/hello_handler.go
@@ -6,7 +6,11 @@ import (
 	"github.com/go-openapi/runtime/middleware"
 )
 
-func PostHello(p operations.PostGreetingParams) middleware.Responder {
+func GetHello(p operations.GetGreetingParams) middleware.Responder {
 	// p.Nameは*string型なので、文字列を取得したいときは*をつけてデリファレンスする
+	return operations.NewGetGreetingOK().WithPayload(*p.Name)
+}
+
+func PostHello(p operations.PostGreetingParams) middleware.Responder {
 	return operations.NewGetGreetingOK().WithPayload(*p.Greeting.Name)
 }
